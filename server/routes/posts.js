f13def3670da770902b9de53226b935eb6fd4300
manishkumar73 <53996222+manishkumar73@users.noreply.github.com>
2021-09-14 22:51:38 +0530

    Add files via upload

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,13 @@
 import express from 'express';
-
-import { getPosts, getPost, createPost, updatePost, likePost, deletePost } from '../controllers/posts.js';
+ 
+import {getPost, getPosts, getPostsBySearch, createPost, updatePost, likePost, deletePost } from '../controllers/posts.js';
 
 const router = express.Router();
 import auth from "../middleware/auth.js";
+
+router.get('/search', getPostsBySearch);
+
+router.get('/:id', getPost)
 
 router.get('/', getPosts);
 router.post('/',auth,  createPost);
